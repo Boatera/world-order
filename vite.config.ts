274a39dc0f3e90da6d4b16d5c41859e6f93113cb
boatera <wijprisub@gmail.com>
2026-08-01 14:35:04 +0700

    Add anti-caching meta tags and build timestamp hash in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   base: '/world-order/',
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+        chunkFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+        assetFileNames: `assets/[name]-[hash]-${Date.now()}[extname]`
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
